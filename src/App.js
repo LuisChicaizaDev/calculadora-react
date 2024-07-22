8822a015e5fda41f035a6ac199459d19316c5111
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2024-07-22 13:08:45 +0200

    feat: componente botones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import logoLuis from './img/logo-luis-developer.png';
+import Button from './components/Button';
 
+// Componente Button con props.children
 function App() {
   return (
     <div className="App">
@@ -12,11 +14,33 @@ function App() {
       </div>
 
       <div className='contenedor-calculadora'>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='row'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='row'>
+          
+        </div>
       </div>
 
     </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; // en versiones actuales se puede omitir
+import '../stylesheets/button.css';
+
+/* props.children los componentes tendrán etiquetas de apertura y cierre
+* Todo lo que esté dentro de la etiqueta sera un hijo del componente
+*/
+function Button(props){
+
+  // Función para saber si es un operador o no 
+  const isOperator = valor => {
+    return isNaN(valor) && (valor != '.' ) && (valor != '=');
+  }
+
+    return(
+        <div className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
+          {props.children}
+        </div>
+    );
+}
+
+export default Button;
