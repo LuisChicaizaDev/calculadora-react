d691efc7e1dfdfee7df326f967c4ef1298ad9da1
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2024-07-22 12:25:14 +0200

    feat: estilo calculadora

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import logoLuis from './img/logo-luis-developer.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img 
+          src= {logoLuis}
+          className='logo-luis'
+          alt = 'Logo Luis' />
+      </div>
+
+      <div className='contenedor-calculadora'>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </div>
+
     </div>
   );
 }
